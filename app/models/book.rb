57064576ad8f74d0daa419c9c98bdd550280cc6a
Arjun Venkataswamy <arjun@example.com>
2015-02-06 15:08:54 -0600

    adds validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,14 @@
 class Book < ActiveRecord::Base
+  validates :title, :author, :num_pages, presence: true
+  validates :num_pages, numericality: { only_integer: true }
+  validate :author_must_be_high_quality
+
+  # validates :author, format: { without: /stephanie meyer/, message: "Can't be Stephanie Meyer"}
+
+  def author_must_be_high_quality
+    if author == "stephanie meyer"
+      errors.add(:author, "must be high quality")
+    end
+
+  end
 end
